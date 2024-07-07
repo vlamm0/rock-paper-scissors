0b1332263ac63bdb3a59ea7b24b09db4f8fef723
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-07 19:02:04 -0400

    Add computer choice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
 // script for rock paper scissors
+
+// computer chooses rock, paper, or scissors
+function getComputerChoice () {
+    // randomly choose decimal 0-1
+    var randomDecimal = Math.random()
+
+    // equally assign decimal to choices 
+    if (randomDecimal < 0.33) {
+        return "rock";
+    }
+    else if (randomDecimal < 0.66) {
+        return "paper";
+    }
+    else {
+        return "scissors"
+    }
+
+}
+
+console.log(getComputerChoice())
